1ac013e11df3e54e5bdb3cf6905a35fba70398e2
Kelly Brant <kelly@redleopard.com>
2018-05-08 16:02:01 -0700

    formatting

--- src/main/java/org/example/csrf/Application.java
+++ src/main/java/org/example/csrf/Application.java
@@ -9,13 +9,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(new Class[] { Application.class, WebConfig.class }, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(new Class[] { Application.class, WebConfig.class }, args);
+	}
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 
 }
